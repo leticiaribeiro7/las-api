9e7756148cf400f4158b12a55ac3b0c900a1de8a
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-06-03 21:48:47 -0300

    modify custom express

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const consign = require("consign");
 
+
+const ENV = process.env.NODE_ENV;
+
 module.exports = () => {
 
     const app = express();
@@ -10,6 +13,26 @@ module.exports = () => {
     consign()
     .include("./src/controllers")
     .into(app);
+    
+    consign().include("src/controllers").into(app);
+
+    app.get("/", (_req, res) => {
+        res.send("Bem vindo ao Las Api");
+    });
+
+
+    // eslint-disable-next-line no-unused-vars
+    app.use((err, req, res, next) => {
+        if (err) {
+            if (err.erroApp) {
+                res.status(400).send(err.erroApp);
+            } else if (ENV !== "production") {
+                res.status(500).send({ error: err.message });
+            }
+        } else {
+            res.status(500).send({ error: "Algo deu errado..." });
+        }
+    });
 
     return app;
 };
