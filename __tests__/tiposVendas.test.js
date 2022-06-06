89e47d63b0d1793320fa59219561bba1eb09503e
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-06-06 15:21:53 -0300

    modify tiposvendas.test

--- __tests__/tiposVendas.test.js
+++ __tests__/tiposVendas.test.js
@@ -41,13 +41,22 @@ describe("API de vendas", () => {
     });
 
 
-    // test("Alterar venda", async () => {
-    //     const alteracao = {
-    //         descricao: "Ingresso"
-    //     };
-    //     const resp = await request.put("/eventos/2").send(alteracao);
-    //     expect(resp.statusCode).toBe(200);
-    // });
+    test("Alterar venda", async () => {
+        const alteracao = {
+            descricao: "Ingresso"
+        };
+        const resp = await request.put("/eventos/2").send(alteracao);
+        expect(resp.body).toEqual({
+            fieldCount: 0,
+            affectedRows: 1,
+            insertId: 0,
+            serverStatus: 2,
+            warningCount: 0,
+            message: "(Rows matched: 1  Changed: 0  Warnings: 0",
+            protocol41: true,
+            changedRows: 0
+          });
+    });
 
     test("Deletar venda", async () => {
         const resp = await request.delete("/tipos-vendas/1");
